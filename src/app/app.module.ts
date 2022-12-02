1b91a27178301188fc8ff40f99bd65eb32d39e5f
Jakub Jóźwiak <jozwiak.ja@icloud.com>
2022-12-02 18:40:57 +0100

    Build, styled components
    Learn how to build and styled components in Angular. I created basic structure by CLI, added two new component, and styled them.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,13 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { SuccessComponent } from './success/app.component';
+import { WarningComponent } from './warning/warning.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
+  declarations: [AppComponent, WarningComponent, SuccessComponent],
+  imports: [BrowserModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
---
+++ src/app/warning/warning.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: '[app-warning]',
+  template: `<h4 class="title">This is warning component</h4> `,
+  styleUrls: ['./warning.component.scss'],
+})
+export class WarningComponent {}
